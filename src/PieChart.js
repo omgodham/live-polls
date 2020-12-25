148e4e929ecc40fdba8625784ebe7cc3dc2bf826
abc <abc@example.com>
2020-12-25 17:31:26 +0530

    Charts will be update on value of dropdown except for global

--- src/PieChart.js
+++ src/PieChart.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./PieChart.css";
 import Chart from "react-google-charts";
 import { useStateValue } from "./StateProvider";
 
 export default function PieChart({ title }) {
   const [{ array }, dispatch] = useStateValue();
-  const channels = array.data.options;
-
+  // const channels = array.data.options;
+const [channels,setChannels]= useState([]);
   var dropDownOptions = [];
   var currentArray = {};
 
@@ -18,29 +18,57 @@ export default function PieChart({ title }) {
     dropDownOptions = [];
   }
 
-  var subArray = {};
-  
-  function handleChange(event) {
-    const value = event.target.value;
+  // var subArray = {};
+  const [subArray,setSubArray] = useState({});
+  const [value,setValue]=useState(`${dropDownOptions[0]}`);
+
+  useEffect(() => {
+    setChannels(array.data.options);
+    console.log(channels);
+  },[]);
+
+  useEffect(() => {
+    // subArray = currentArray[value];
+    if(title != 'global')
+    setSubArray(currentArray[value]);
+    else
+    setSubArray(currentArray[Object.keys(currentArray)[0]]);
     console.log(value);
-    subArray = currentArray[value];
-    console.log(subArray);
-    data[0] = ["Name Of Channel", "Public Voting"];
-    for (var i = 0; i < Object.keys(channels).length; i++) {
-      data[i + 1] = [channels[i], subArray[i]];
-    }
-    console.log(data);
+  },[value]);
+  // console.log("after");
+
+
+
+  function handleChange(event) {
+    // value = event.target.value;
+    setValue(event.target.value);
+    // console.log(value);
+    // subArray = currentArray[value];
+    // console.log(subArray);
+    // data[0] = ["Name Of Channel", "Public Voting"];
+    // for (var i = 0; i < Object.keys(channels).length; i++) {
+    //   data[i + 1] = [channels[i], subArray[i]];
+    // }
+    // console.log(data);
   }
-  
-  if(title!='global')
-  subArray = currentArray[Object.keys(currentArray)[0]];
-  else
-  subArray =currentArray;
-  var data = [];
+
+  // if(title!='global')
+  // subArray = currentArray[Object.keys(currentArray)[0]];
+  // // else
+  // subArray =currentArray;
+   var data = [];
+
+  // subArray = currentArray[value];
+  console.log(subArray);
   data[0] = ["Name Of Channel", "Public Voting"];
   for (var i = 0; i < Object.keys(channels).length; i++) {
     data[i + 1] = [channels[i], subArray[i]];
   }
+
+  // data[0] = ["Name Of Channel", "Public Voting"];
+  // for (var i = 0; i < Object.keys(channels).length; i++) {
+  //   data[i + 1] = [channels[i], subArray[i]];
+  // }
 
 
   var options = {
