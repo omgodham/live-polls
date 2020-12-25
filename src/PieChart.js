cd5a7d908ad2760127328849ed759443c4c981b2
abc <abc@example.com>
2020-12-25 16:52:27 +0530

    Displayed Static pie-charts(for initial values only)

--- src/PieChart.js
+++ src/PieChart.js
@@ -4,49 +4,68 @@ import Chart from "react-google-charts";
 import { useStateValue } from "./StateProvider";
 
 export default function PieChart({ title }) {
-    
+  const [{ array }, dispatch] = useStateValue();
+  const channels = array.data.options;
 
-  const [{ array}, dispatch] = useStateValue();
-    const dropDown=array.data[title];
-    const dropDownOptions=Object.keys(dropDown);
- 
+  var dropDownOptions = [];
+  var currentArray = {};
 
-  // var data = [];
+  if (title != "global") {
+    currentArray = array.data[title];
+    dropDownOptions = Object.keys(currentArray);
+  } else {
+    currentArray = array.data[title];
+    dropDownOptions = [];
+  }
 
-  //  data[0]=["Name Of Channel","Public Voting"];
-  //     for(var i=0;i<Object.keys(channels).length;i++)
-  // {
-  //     data[i+1]=[channels[i],results[i]];
-  //     console.log(data[i+1]);
-  // }
- 
+  var subArray = {};
+  
+  function handleChange(event) {
+    const value = event.target.value;
+    console.log(value);
+    subArray = currentArray[value];
+    console.log(subArray);
+    data[0] = ["Name Of Channel", "Public Voting"];
+    for (var i = 0; i < Object.keys(channels).length; i++) {
+      data[i + 1] = [channels[i], subArray[i]];
+    }
+    console.log(data);
+  }
+  
+  if(title!='global')
+  subArray = currentArray[Object.keys(currentArray)[0]];
+  else
+  subArray =currentArray;
+  var data = [];
+  data[0] = ["Name Of Channel", "Public Voting"];
+  for (var i = 0; i < Object.keys(channels).length; i++) {
+    data[i + 1] = [channels[i], subArray[i]];
+  }
 
-  // const options = {
-  //   title: "My Daily Activities",
-  //   pieHole: 0.4,
-  //   is3D: false
-  // };
-   function handleChange(event){
-      const value=event.target.value;
-      console.log(value);
-   }
+
+  var options = {
+    title: "",
+    pieHole: 0.4,
+    is3D: false,
+  };
 
   return (
     <div>
       <h4>{title}</h4>
-      
-        <select  onChange={handleChange}>
-            {dropDownOptions.map( item => {
-                return <option>{item}</option>
-            })}
-        </select>
-      {/* <Chart
-          chartType="PieChart"
-          width="100%"
-          height="400px"
-          data={data}
-          options={options}
-        /> */}
+
+      <select onChange={handleChange}>
+        {dropDownOptions.map((item) => {
+          return <option>{item}</option>;
+        })}
+      </select>
+
+      <Chart
+        chartType="PieChart"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+      />
     </div>
   );
 }
