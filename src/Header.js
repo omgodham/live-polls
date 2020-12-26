a41b34f5807d2c9757e0a541f3fd9986ca751dd4
abc <abc@example.com>
2020-12-26 12:12:15 +0530

    for hosting

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,13 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import Logo from "./logo.png";
 import "./Header.css";
 function Header() {
     return (
         <div className="header">
             <div className="header__content">
             <div className="heaeder__logo">
-            <img className="header__logo__image" src="http://demo.themeton.com/pressgrid/wp-content/uploads/sites/73/2017/01/logo.png"/>
+            <img className="header__logo__image" src={Logo}/>
             </div>
             <div className="header__nav">
             <ul className="header__menu">
